91755beeb8a0f6d3866b9b452bb6fe582a37df27
Muki <michal@mkiol.net>
2023-04-08 16:24:03 +0200

    swedish translation

---
+++ translations/dsnote-sv.ts
@@ -0,0 +1,446 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="sv" sourcelanguage="en_US">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../sfos/qml/AboutPage.qml" line="28"/>
+        <source>About %1</source>
+        <translation>Om %1</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/AboutPage.qml" line="46"/>
+        <source>Version %1</source>
+        <translation>Version %1</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/AboutPage.qml" line="53"/>
+        <source>Project website</source>
+        <translation>Projektsida</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/AboutPage.qml" line="57"/>
+        <source>Changes</source>
+        <translation>Ändringar</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/AboutPage.qml" line="63"/>
+        <source>Authors</source>
+        <translation>Utvecklare</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/AboutPage.qml" line="77"/>
+        <source>%1 is developed as an open source project under %2.</source>
+        <translation>%1 är utvecklad som ett öppen källkodsprojekt under %2.</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/AboutPage.qml" line="83"/>
+        <source>Translators</source>
+        <translation>Översättare</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/AboutPage.qml" line="92"/>
+        <source>Libraries in use</source>
+        <translation>Bibliotek som används</translation>
+    </message>
+</context>
+<context>
+    <name>ChangelogPage</name>
+    <message>
+        <location filename="../sfos/qml/ChangelogPage.qml" line="28"/>
+        <source>Changes</source>
+        <translation>Ändringar</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/ChangelogPage.qml" line="32"/>
+        <location filename="../sfos/qml/ChangelogPage.qml" line="87"/>
+        <location filename="../sfos/qml/ChangelogPage.qml" line="125"/>
+        <location filename="../sfos/qml/ChangelogPage.qml" line="158"/>
+        <location filename="../sfos/qml/ChangelogPage.qml" line="181"/>
+        <location filename="../sfos/qml/ChangelogPage.qml" line="195"/>
+        <location filename="../sfos/qml/ChangelogPage.qml" line="209"/>
+        <location filename="../sfos/qml/ChangelogPage.qml" line="219"/>
+        <source>Version %1</source>
+        <translation>Version %1</translation>
+    </message>
+</context>
+<context>
+    <name>DirPage</name>
+    <message>
+        <location filename="../sfos/qml/DirPage.qml" line="61"/>
+        <source>SD Card</source>
+        <translation>SD-kort</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/DirPage.qml" line="66"/>
+        <source>Home</source>
+        <translation>Hem</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/DirPage.qml" line="82"/>
+        <source>No directories</source>
+        <translation>Inga mappar</translation>
+    </message>
+</context>
+<context>
+    <name>LangItem</name>
+    <message>
+        <location filename="../sfos/qml/LangItem.qml" line="34"/>
+        <source>Show models</source>
+        <translation>Visa modeller</translation>
+    </message>
+</context>
+<context>
+    <name>LangsPage</name>
+    <message>
+        <location filename="../sfos/qml/LangsPage.qml" line="41"/>
+        <location filename="../desktop/qml/LangsPage.qml" line="19"/>
+        <source>Languages</source>
+        <translation>Språk</translation>
+    </message>
+    <message>
+        <location filename="../desktop/qml/LangsPage.qml" line="99"/>
+        <source>Delete</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message>
+        <location filename="../desktop/qml/LangsPage.qml" line="99"/>
+        <source>Download</source>
+        <translation>Ladda ner</translation>
+    </message>
+    <message>
+        <location filename="../desktop/qml/LangsPage.qml" line="99"/>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+</context>
+<context>
+    <name>ModelItem</name>
+    <message>
+        <location filename="../sfos/qml/ModelItem.qml" line="42"/>
+        <source>Set as default for this language</source>
+        <translation>Ange som standard för detta språk</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/ModelItem.qml" line="49"/>
+        <source>Delete</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/ModelItem.qml" line="49"/>
+        <source>Download</source>
+        <translation>Ladda ner</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/ModelItem.qml" line="49"/>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+</context>
+<context>
+    <name>NotesPage</name>
+    <message>
+        <location filename="../sfos/qml/NotesPage.qml" line="41"/>
+        <source>About %1</source>
+        <translation>Om %1</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/NotesPage.qml" line="46"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/NotesPage.qml" line="54"/>
+        <location filename="../desktop/qml/NotesPage.qml" line="144"/>
+        <source>Transcribe audio file</source>
+        <translation>Transkribera ljudfil</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/NotesPage.qml" line="136"/>
+        <location filename="../sfos/qml/NotesPage.qml" line="146"/>
+        <location filename="../desktop/qml/NotesPage.qml" line="69"/>
+        <location filename="../desktop/qml/NotesPage.qml" line="143"/>
+        <source>Transcribing audio file...</source>
+        <translation>Transkriberar ljudfil...</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/NotesPage.qml" line="67"/>
+        <location filename="../desktop/qml/NotesPage.qml" line="132"/>
+        <source>Clear</source>
+        <translation>Rensa</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/NotesPage.qml" line="73"/>
+        <source>Copy</source>
+        <translation>Kopiera</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/NotesPage.qml" line="99"/>
+        <location filename="../sfos/qml/NotesPage.qml" line="142"/>
+        <source>Language is not configured</source>
+        <translation>Språket är inte konfigurerat</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/NotesPage.qml" line="100"/>
+        <source>Pull down and select Settings to download language</source>
+        <translation>Dra neråt och välj &quot;Inställningar&quot; för att ladda ner språk</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/NotesPage.qml" line="141"/>
+        <source>Starting...</source>
+        <translation>Startar...</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/NotesPage.qml" line="135"/>
+        <location filename="../sfos/qml/NotesPage.qml" line="145"/>
+        <location filename="../desktop/qml/NotesPage.qml" line="73"/>
+        <location filename="../desktop/qml/NotesPage.qml" line="75"/>
+        <source>Say something...</source>
+        <translation>Säg något...</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/NotesPage.qml" line="138"/>
+        <location filename="../desktop/qml/NotesPage.qml" line="80"/>
+        <source>Press and say something...</source>
+        <translation>Håll ner och säg något...</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/NotesPage.qml" line="137"/>
+        <location filename="../desktop/qml/NotesPage.qml" line="79"/>
+        <source>Click and say something...</source>
+        <translation>Snabbtryck och säg något...</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/NotesPage.qml" line="133"/>
+        <location filename="../sfos/qml/NotesPage.qml" line="143"/>
+        <location filename="../desktop/qml/NotesPage.qml" line="77"/>
+        <source>Decoding, please wait...</source>
+        <translation>Avkodar, vänta...</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/NotesPage.qml" line="134"/>
+        <location filename="../sfos/qml/NotesPage.qml" line="144"/>
+        <location filename="../desktop/qml/NotesPage.qml" line="67"/>
+        <source>Getting ready, please wait...</source>
+        <translation>Förbereder, vänta...</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/NotesPage.qml" line="147"/>
+        <source>Busy...</source>
+        <translation>Upptagen...</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/NotesPage.qml" line="187"/>
+        <source>Audio file couldn&apos;t be transcribed.</source>
+        <translation>Ljudfilen kunde inte transkriberas.</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/NotesPage.qml" line="190"/>
+        <source>Microphone was unexpectedly disconnected.</source>
+        <translation>Mikrofonen kopplades oväntat bort.</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/NotesPage.qml" line="193"/>
+        <source>Unable to start service.</source>
+        <translation>Kunde inte starta tjänsten.</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/NotesPage.qml" line="196"/>
+        <source>Oops! Something went wrong.</source>
+        <translation>Hoppsan! Något gick åt pipsvängen.</translation>
+    </message>
+    <message>
+        <location filename="../desktop/qml/NotesPage.qml" line="19"/>
+        <source>Note</source>
+        <translation>Anteckning</translation>
+    </message>
+    <message>
+        <location filename="../desktop/qml/NotesPage.qml" line="103"/>
+        <source>Press and hold to start listening</source>
+        <translation>Tryck och håll kvar för att lyssna</translation>
+    </message>
+    <message>
+        <location filename="../desktop/qml/NotesPage.qml" line="120"/>
+        <source>Click to start listening</source>
+        <translation>Snabbtryck för att lyssna</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/NotesPage.qml" line="54"/>
+        <location filename="../desktop/qml/NotesPage.qml" line="120"/>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <location filename="../desktop/qml/NotesPage.qml" line="158"/>
+        <source>No language is configured</source>
+        <translation>Inget språk är konfigurerat</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../sfos/qml/SettingsPage.qml" line="30"/>
+        <location filename="../desktop/qml/SettingsPage.qml" line="18"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/SettingsPage.qml" line="42"/>
+        <source>Active language</source>
+        <translation>Aktivt språk</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/SettingsPage.qml" line="76"/>
+        <source>Languages</source>
+        <translation>Språk</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/SettingsPage.qml" line="84"/>
+        <location filename="../desktop/qml/SettingsPage.qml" line="38"/>
+        <source>Listening mode</source>
+        <translation>Lyssningsläge</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/SettingsPage.qml" line="91"/>
+        <location filename="../desktop/qml/SettingsPage.qml" line="47"/>
+        <source>One sentence</source>
+        <translation>En mening</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/SettingsPage.qml" line="92"/>
+        <location filename="../desktop/qml/SettingsPage.qml" line="48"/>
+        <source>Press and hold</source>
+        <translation>Tryck och håll kvar</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/SettingsPage.qml" line="93"/>
+        <location filename="../desktop/qml/SettingsPage.qml" line="49"/>
+        <source>Always on</source>
+        <translation>Alltid på</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/SettingsPage.qml" line="104"/>
+        <source>One sentence: Clicking on the bottom panel starts listening, which ends when the first sentence is recognized.
+Press and hold: Pressing and holding on the bottom panel enables listening. When you stop holding, listening will turn off.
+Always on: Listening is always turn on.</source>
+        <translation>En mening: Börjar lyssna vid snabbtryck på den nedre panelen och slutar när första meningen identifierats.
+Tryck och håll kvar: Börjar lyssna när du trycker ner och håller kvar på nedre panelen, slutar lyssna när du släpper.
+Alltid på: Lyssning alltid påslagen.</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/SettingsPage.qml" line="110"/>
+        <location filename="../desktop/qml/SettingsPage.qml" line="66"/>
+        <source>Text appending style</source>
+        <translation>Stil för att lägga till text</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/SettingsPage.qml" line="117"/>
+        <location filename="../desktop/qml/SettingsPage.qml" line="75"/>
+        <source>In line</source>
+        <translation>På rad</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/SettingsPage.qml" line="118"/>
+        <location filename="../desktop/qml/SettingsPage.qml" line="76"/>
+        <source>After line break</source>
+        <translation>Efter radbrytning</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/SettingsPage.qml" line="129"/>
+        <source>Text is appended to the note in the same line or after line break.</source>
+        <translation>Texten läggs till i anteckningen på samma rad eller efter radbrytning.</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/SettingsPage.qml" line="136"/>
+        <location filename="../desktop/qml/SettingsPage.qml" line="108"/>
+        <source>Translate to English</source>
+        <translation>Översätt till Engelska</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/SettingsPage.qml" line="137"/>
+        <source>Translate decoded text to English. This option works only with Whisper models.</source>
+        <translation>Översätter avkodad text till Engelska. Detta alternativ fungerar endast med Whisper-modeller.</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/SettingsPage.qml" line="144"/>
+        <location filename="../desktop/qml/SettingsPage.qml" line="92"/>
+        <source>Location of language files</source>
+        <translation>Lagringsplats för språkfiler</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/SettingsPage.qml" line="146"/>
+        <source>Directory where language files are downloaded to and stored.</source>
+        <translation>Mapp dit språkfiler laddas ner och lagras.</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/SettingsPage.qml" line="150"/>
+        <location filename="../desktop/qml/SettingsPage.qml" line="101"/>
+        <source>Change</source>
+        <translation>Ändra</translation>
+    </message>
+    <message>
+        <location filename="../sfos/qml/SettingsPage.qml" line="159"/>
+        <source>Set default</source>
+        <translation>Ange standard</translation>
+    </message>
+    <message>
+        <location filename="../desktop/qml/SettingsPage.qml" line="116"/>
+        <source>Please choose a directory</source>
+        <translation>Välj en mapp</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../desktop/qml/main.qml" line="49"/>
+        <source>Languages configuration</source>
+        <translation>Språkkonfiguration</translation>
+    </message>
+    <message>
+        <location filename="../desktop/qml/main.qml" line="57"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <location filename="../desktop/qml/main.qml" line="66"/>
+        <source>Quit</source>
+        <translation>Avsluta</translation>
+    </message>
+</context>
+<context>
+    <name>stt_service</name>
+    <message>
+        <location filename="../src/stt_service.cpp" line="869"/>
+        <source>Language is not configured</source>
+        <translation>Språket är inte konfigurerat</translation>
+    </message>
+    <message>
+        <location filename="../src/stt_service.cpp" line="870"/>
+        <source>Say something...</source>
+        <translation>Säg något...</translation>
+    </message>
+    <message>
+        <location filename="../src/stt_service.cpp" line="872"/>
+        <source>Press and say something...</source>
+        <translation>Håll ner och säg något...</translation>
+    </message>
+    <message>
+        <location filename="../src/stt_service.cpp" line="874"/>
+        <source>Click and say something...</source>
+        <translation>Snabbtryck och säg något...</translation>
+    </message>
+    <message>
+        <location filename="../src/stt_service.cpp" line="875"/>
+        <source>Busy...</source>
+        <translation>Upptagen...</translation>
+    </message>
+    <message>
+        <location filename="../src/stt_service.cpp" line="876"/>
+        <source>Decoding, please wait...</source>
+        <translation>Avkodar, vänta...</translation>
+    </message>
+    <message>
+        <location filename="../src/stt_service.cpp" line="878"/>
+        <source>Getting ready, please wait...</source>
+        <translation>Förbereder, vänta...</translation>
+    </message>
+</context>
+</TS>
