f196170c94a2d528b676c1de18c58c796ae17a3e
mkiol <michal@mkiol.net>
2023-09-20 19:33:27 +0200

    app icon in png format

--- cmake/install_desktop.cmake
+++ cmake/install_desktop.cmake
@@ -1,8 +1,16 @@
 install(TARGETS ${info_binary_id} RUNTIME DESTINATION bin)
 
 install(FILES "${desktop_dir}/${info_binary_id}.desktop" DESTINATION share/applications)
-install(FILES "${desktop_dir}/${info_binary_id}.svg" DESTINATION share/icons/hicolor/scalable/apps)
 install(FILES "${desktop_dir}/${info_binary_id}.metainfo.xml" DESTINATION share/metainfo)
+install(FILES "${desktop_dir}/${info_binary_id}.svg" DESTINATION share/icons/hicolor/scalable/apps)
+install(FILES "${desktop_dir}/icons/16x16/${info_binary_id}.png" DESTINATION share/icons/hicolor/16x16/apps)
+install(FILES "${desktop_dir}/icons/32x32/${info_binary_id}.png" DESTINATION share/icons/hicolor/32x32/apps)
+install(FILES "${desktop_dir}/icons/48x48/${info_binary_id}.png" DESTINATION share/icons/hicolor/48x48/apps)
+install(FILES "${desktop_dir}/icons/64x64/${info_binary_id}.png" DESTINATION share/icons/hicolor/64x64/apps)
+install(FILES "${desktop_dir}/icons/96x96/${info_binary_id}.png" DESTINATION share/icons/hicolor/96x96/apps)
+install(FILES "${desktop_dir}/icons/128x128/${info_binary_id}.png" DESTINATION share/icons/hicolor/128x128/apps)
+install(FILES "${desktop_dir}/icons/256x256/${info_binary_id}.png" DESTINATION share/icons/hicolor/256x256/apps)
+install(FILES "${desktop_dir}/icons/512x512/${info_binary_id}.png" DESTINATION share/icons/hicolor/512x512/apps)
 
 if(BUILD_WHISPERCPP)
     install(FILES "${external_lib_dir}/libwhisper-openblas.so" DESTINATION lib)
