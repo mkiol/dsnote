9d6a9ba6644640514b97100e980e5b0c5f5134de
mkiol <michal@mkiol.net>
2023-10-01 11:02:20 +0200

    fix: opencl was disabled

--- src/gpu_tools.cpp
+++ src/gpu_tools.cpp
@@ -217,7 +217,7 @@ std::vector<gpu_tools::device> available_devices() {
 
     add_cuda_devices(devices);
     add_hip_devices(devices);
-    // add_opencl_devices(devices);
+    add_opencl_devices(devices);
 
     return devices;
 }
