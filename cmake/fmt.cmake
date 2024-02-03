bcca7f6039768175f31934fa2737dd349e287c7d
mkiol <michal@mkiol.net>
2024-02-03 15:37:25 +0100

    update libfmt to version 10.2.1

--- cmake/fmt.cmake
+++ cmake/fmt.cmake
@@ -1,12 +1,12 @@
-set(fmt_source_url "https://github.com/fmtlib/fmt/releases/download/9.1.0/fmt-9.1.0.zip")
-set(fmt_checksum "6133244fe8ef6f75c5601e8069b37b04")
+set(fmt_source_url "https://github.com/fmtlib/fmt/releases/download/10.2.1/fmt-10.2.1.zip")
+set(fmt_checksum "312151a2d13c8327f5c9c586ac6cf7cddc1658e8f53edae0ec56509c8fa516c9")
 
 ExternalProject_Add(fmt
     SOURCE_DIR ${external_dir}/fmt
     BINARY_DIR ${PROJECT_BINARY_DIR}/external/fmt
     INSTALL_DIR ${PROJECT_BINARY_DIR}/external
     URL "${fmt_source_url}"
-    URL_MD5 "${fmt_checksum}"
+    URL_HASH SHA256=${fmt_checksum}
     CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/ -DCMAKE_INSTALL_LIBDIR=/lib
         -DCMAKE_INSTALL_INCLUDEDIR=/include -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DFMT_DOC=OFF
         -DFMT_TEST=OFF -DFMT_FUZZ=OFF
