03a6653737acd4db8b4a524672141361bc063396
mkiol <michal@mkiol.net>
2024-04-20 20:42:54 +0200

    update openblas
    0.3.21 => 0.3.26

--- cmake/openblas.cmake
+++ cmake/openblas.cmake
@@ -1,5 +1,5 @@
-set(openblas_source_url "https://github.com/xianyi/OpenBLAS/releases/download/v0.3.21/OpenBLAS-0.3.21.tar.gz")
-set(openblas_checksum "ffb6120e2309a2280471716301824805")
+set(openblas_source_url "https://github.com/OpenMathLib/OpenBLAS/releases/download/v0.3.26/OpenBLAS-0.3.26.tar.gz")
+set(openblas_checksum "4e6e4f5cb14c209262e33e6816d70221a2fe49eb69eaf0a06f065598ac602c68")
 
 set(openblas_opts
     -DCMAKE_BUILD_TYPE=Release
@@ -25,8 +25,8 @@ ExternalProject_Add(openblas
     SOURCE_DIR ${external_dir}/openblas
     BINARY_DIR ${PROJECT_BINARY_DIR}/external/openblas
     INSTALL_DIR ${PROJECT_BINARY_DIR}/external
-    URL "${openblas_source_url}"
-    URL_MD5 "${openblas_checksum}"
+    URL ${openblas_source_url}
+    URL_HASH SHA256=${openblas_checksum}
     CMAKE_ARGS ${openblas_opts}
     BUILD_ALWAYS False
 )
