f4604ca7c9e527fef68a118e7a221a960522a2b0
mkiol <michal@mkiol.net>
2026-02-24 16:42:41 +0100

    fix: qt6 dbus xml gen failed

--- cmake/dbus_api.cmake
+++ cmake/dbus_api.cmake
@@ -2,25 +2,18 @@ set(dbus_dsnote_interface_file "${PROJECT_BINARY_DIR}/${info_dbus_app_interface}
 
 configure_file(${dbus_dir}/dsnote.xml.in ${dbus_dsnote_interface_file})
 
-find_package(Qt6 COMPONENTS DBus REQUIRED)
-
-unset(qdbusxml2cpp_bin CACHE)
-find_program(qdbusxml2cpp_bin NAMES qdbusxml2cpp qdbusxml2cpp-qt6 HINTS ${Qt6_DIR}/../../../bin /usr/lib/qt6/bin)
-if(NOT qdbusxml2cpp_bin)
-   message(FATAL_ERROR "qdbusxml2cpp not found but it is required")
-endif()
+find_package(Qt${QT_VERSION_MAJOR} COMPONENTS DBus REQUIRED)
 
 add_custom_command(
     OUTPUT dbus_dsnote_adaptor.h dbus_dsnote_adaptor.cpp
-    COMMAND ${qdbusxml2cpp_bin} ${info_dbus_app_interface}.xml -a dbus_dsnote_adaptor -c DsnoteAdaptor
-    DEPENDS ${info_dbus_app_interface}.xml
+    COMMAND ${QT_CMAKE_EXPORT_NAMESPACE}::qdbusxml2cpp ${info_dbus_app_interface}.xml -a dbus_dsnote_adaptor -c DsnoteAdaptor
+    DEPENDS ${info_dbus_app_interface}.xml ${QT_CMAKE_EXPORT_NAMESPACE}::qdbusxml2cpp
     COMMENT "generate dbus app adaptor sources"
 )
-
 add_custom_command(
     OUTPUT dbus_dsnote_inf.h dbus_dsnote_inf.cpp
-    COMMAND ${qdbusxml2cpp_bin} ${info_dbus_app_interface}.xml -p dbus_dsnote_inf -c DsnoteDbusInterface
-    DEPENDS ${info_dbus_app_interface}.xml
+    COMMAND ${QT_CMAKE_EXPORT_NAMESPACE}::qdbusxml2cpp ${info_dbus_app_interface}.xml -p dbus_dsnote_inf -c DsnoteDbusInterface
+    DEPENDS ${info_dbus_app_interface}.xml ${QT_CMAKE_EXPORT_NAMESPACE}::qdbusxml2cpp
     COMMENT "generate dbus app inf sources"
 )
 
