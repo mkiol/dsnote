6912e65ffc4542bb87beed36fc8f803081087dac
mkiol <michal@mkiol.net>
2026-05-31 18:25:18 +0200

    update pybind11 to ver 3.0.4

--- cmake/pybind11.cmake
+++ cmake/pybind11.cmake
@@ -1,5 +1,5 @@
-set(pybind11_source_url "https://github.com/pybind/pybind11/archive/refs/tags/v2.13.6.tar.gz")
-set(pybind11_checksum "e08cb87f4773da97fa7b5f035de8763abc656d87d5773e62f6da0587d1f0ec20")
+set(pybind11_source_url "https://github.com/pybind/pybind11/archive/refs/tags/v3.0.4.tar.gz")
+set(pybind11_checksum "74b6a2c2b4573a400cafb6ecbf60c98df300cd3d0041296b913d02b2cbbb2676")
 
 ExternalProject_Add(pybind11
     SOURCE_DIR ${external_dir}/pybind11
@@ -10,7 +10,7 @@ ExternalProject_Add(pybind11
     DOWNLOAD_EXTRACT_TIMESTAMP TRUE
     CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DCMAKE_INSTALL_LIBDIR=${external_lib_dir}
         -DCMAKE_INSTALL_INCLUDEDIR=${external_include_dir} -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_SHARED_LIBS=OFF
-        -DPYBIND11_INSTALL=ON -DPYBIND11_TEST=OFF -DPYBIND11_FINDPYTHON=ON
+        -DPYBIND11_INSTALL=ON -DPYBIND11_TEST=OFF -DPYBIND11_FINDPYTHON=OFF -DPYBIND11_NOPYTHON=ON
     BUILD_ALWAYS False
 )
 
