3867351e49d6c6c7acf4e5ffa7c7d896a74e2534
mkiol <michal@mkiol.net>
2026-06-06 19:32:12 +0200

    update espeak

--- cmake/espeak.cmake
+++ cmake/espeak.cmake
@@ -1,8 +1,9 @@
 set(mbrola_source_url "https://github.com/numediart/MBROLA/archive/bf17e9e1416a647979ac683657a536e8ca5d880e.zip")
 set(mbrola_checksum "5a7c02a926dc48ab6d1af0e4c8ab53fc191a7e4337de6df57b6706e140fa3087")
 
-set(espeak_source_url "https://github.com/rhasspy/espeak-ng/archive/8593723f10cfd9befd50de447f14bf0a9d2a14a4.zip")
-set(espeak_checksum "cc8092f23a28ccd79b1c5e62984a4c4ac1959d2d0b8193ac208d728c620bd5ed")
+set(espeak_source_url "https://github.com/espeak-ng/espeak-ng/archive/fbe4b3764285c35b1f035cb8d09ad9fc19f71c30.zip")
+set(espeak_checksum "0879c9daaea2a3777e61a5e2c6c5f2e91b312ab29c562091c99bbfd82d261bee")
+set(espeak_so_version "1.52.0.1")
 
 if(${autoconf_bin} MATCHES "-NOTFOUND$")
    message(FATAL_ERROR "autoconf not found but it is required to build espeak")
@@ -34,18 +35,26 @@ ExternalProject_Add(espeak
     URL ${espeak_source_url}
     URL_HASH SHA256=${espeak_checksum}
     DOWNLOAD_EXTRACT_TIMESTAMP TRUE
-    CONFIGURE_COMMAND cp -r --no-target-directory <SOURCE_DIR> <BINARY_DIR> &&
-        <BINARY_DIR>/autogen.sh &&
-        <BINARY_DIR>/configure --prefix=<INSTALL_DIR> --libdir=<INSTALL_DIR>/lib --with-pic
-        --with-pcaudiolib=no --with-sonic=no --with-speechplayer=no
-        --with-mbrola=yes --enable-static=yes --enable-shared=yes
-        --with-extdict-ru --enable-rpath=no
-    BUILD_COMMAND export LD_LIBRARY_PATH=<BINARY_DIR>/src/.libs && make
+    PATCH_COMMAND patch --batch --unified -p1 --directory=<SOURCE_DIR>
+                -i ${patches_dir}/espeak.patch ||
+                    echo "patch cmd failed, likely already patched"
+    CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release
+        -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DCMAKE_PREFIX_PATH=<INSTALL_DIR>
+        -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+        -DENABLE_TESTS=OFF
+        -DBUILD_SHARED_LIBS=ON
+        -DBUILD_STATIC_LIBS=ON
+        -DUSE_MBROLA=ON
+        -DUSE_LIBSONIC=OFF
+        -DUSE_LIBPCAUDIO=OFF
+        -DUSE_SPEECHPLAYER=OFF
+        -DESPEAK_BUILD_MANPAGES=OFF
     BUILD_ALWAYS False
-    INSTALL_COMMAND make DESTDIR=/ install
 )
 
 ExternalProject_Add_StepDependencies(espeak configure mbrola)
 
-list(APPEND deps_libs "${external_lib_dir}/libespeak-ng.a")
+list(APPEND deps_libs
+        "${external_lib_dir}/libespeak-ng.a"
+        "${external_lib_dir}/libucd.a")
 list(APPEND deps espeak mbrola)
